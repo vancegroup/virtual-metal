fc5d62b4b5b9efb0b1adbf89fb9d89c61df52109
Ryan Pavlik <rpavlik@iastate.edu>
2012-02-08 17:52:20 -0600

    Run lua-styler on code.

--- main.lua
+++ main.lua
@@ -29,15 +29,17 @@ RelativeTo.World:addChild(
 
 require("Text")
 
-RelativeTo.World:addChild(TextGeode{
-	"Training Session",
-	position = {.5, 1.3, 0},
-	lineHeight = .15,
-	font = Font("DroidSansBold"),
-})
+RelativeTo.World:addChild(
+	TextGeode{
+		"Training Session",
+		position = {.5, 1.3, 0},
+		lineHeight = .15,
+		font = Font("DroidSansBold"),
+	}
+)
 
 function getNamedChild(node, name)
-	for i=0,node:getNumChildren() - 1 do
+	for i = 0, node:getNumChildren() - 1 do
 		local child = node:getChild(i)
 		print(child:getName())
 		if child:getName() == name then
@@ -48,7 +50,7 @@ end
 
 components = { }
 
-for i=0,metalmodel:getNumChildren() - 1 do
+for i = 0, metalmodel:getNumChildren() - 1 do
 	local child = metalmodel:getChild(i)
 	components[child:getName()] = child
 end
@@ -73,78 +75,84 @@ pointRadius = 0.0125
 device = gadget.PositionInterface("VJWand")
 
 --[[ Action for switching visibility ]]
-Actions.addFrameAction(function()
-	local switchBtn = gadget.DigitalInterface("VJButton2")
-	while true do
-		components.Screens:setAllChildrenOn()
-		components.Scrim:setAllChildrenOn()
-		components.Projection:setAllChildrenOff()
+Actions.addFrameAction(
+	function()
+		local switchBtn = gadget.DigitalInterface("VJButton2")
+		while true do
+			components.Screens:setAllChildrenOn()
+			components.Scrim:setAllChildrenOn()
+			components.Projection:setAllChildrenOff()
 
-		repeat
-			Actions.waitForRedraw()
-		until switchBtn.justPressed
+			repeat
+				Actions.waitForRedraw()
+			until switchBtn.justPressed
 
-		components.Scrim:setAllChildrenOff()
-		components.Screens:setAllChildrenOff()
-		components.Projection:setAllChildrenOn()
+			components.Scrim:setAllChildrenOff()
+			components.Screens:setAllChildrenOff()
+			components.Projection:setAllChildrenOn()
 
-		repeat
-			Actions.waitForRedraw()
-		until switchBtn.justPressed
+			repeat
+				Actions.waitForRedraw()
+			until switchBtn.justPressed
 
-		components.Projection:setAllChildrenOff()
+			components.Projection:setAllChildrenOff()
 
-		repeat
-			Actions.waitForRedraw()
-		until switchBtn.justPressed
+			repeat
+				Actions.waitForRedraw()
+			until switchBtn.justPressed
 
+		end
 	end
-end)
+)
 
 -- This frame action draws and updates our
 -- cursor at the device's location.
-Actions.addFrameAction(function()
-	local xform = osg.MatrixTransform()
-	xform:addChild(
-		TransparentGroup{
-			alpha = 0.7,
-			Sphere{
-				radius = pointRadius,
-				position = {0, 0, 0}
+Actions.addFrameAction(
+	function()
+		local xform = osg.MatrixTransform()
+		xform:addChild(
+			TransparentGroup{
+				alpha = 0.7,
+				Sphere{
+					radius = pointRadius,
+					position = {0, 0, 0}
+				}
 			}
-		}
-	)
+		)
 
-	RelativeTo.Room:addChild(xform)
+		RelativeTo.Room:addChild(xform)
 
-	-- Update the cursor position forever.
-	while true do
-		xform:setMatrix(device.matrix)
-		Actions.waitForRedraw()
+		-- Update the cursor position forever.
+		while true do
+			xform:setMatrix(device.matrix)
+			Actions.waitForRedraw()
+		end
 	end
-end)
+)
 
 -- This action adds to the scenegraph when you
 -- press/hold a button to draw
-Actions.addFrameAction(function()
-	local drawBtn = gadget.DigitalInterface("VJButton1")
-	while true do
-		while not drawBtn.pressed do
-			Actions.waitForRedraw()
+Actions.addFrameAction(
+	function()
+		local drawBtn = gadget.DigitalInterface("VJButton1")
+		while true do
+			while not drawBtn.pressed do
+				Actions.waitForRedraw()
+			end
+
+			while drawBtn.pressed do
+				local newPoint = osg.PositionAttitudeTransform()
+				newPoint:addChild(Sphere{radius = pointRadius, position = {0, 0, 0}})
+				newPoint:setPosition(device.position - osgnav.position)
+
+				RelativeTo.World:addChild(newPoint)
+
+				Actions.waitForRedraw()
+			end
 		end
 
-		while drawBtn.pressed do
-			local newPoint = osg.PositionAttitudeTransform()
-			newPoint:addChild(Sphere{radius = pointRadius, position = {0, 0, 0}})
-			newPoint:setPosition(device.position - osgnav.position)
-
-			RelativeTo.World:addChild(newPoint)
-
-			Actions.waitForRedraw()
-		end
 	end
-
-end)
+)
 
 
 
