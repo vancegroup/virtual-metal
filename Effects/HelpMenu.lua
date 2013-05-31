34be99c72acafcf578a4997a17bab92e33be671c
Ryan Pavlik <rpavlik@iastate.edu>
2013-05-31 13:54:05 -0500

    Make some variables in help menu local so that we avoid namespace pollution

--- Effects/HelpMenu.lua
+++ Effects/HelpMenu.lua
@@ -1,14 +1,14 @@
 require "Actions"
 require "gldef"
 
-wiihelp = Transform{
+local wiihelp = Transform{
 	position={0,1.3,0},
 	orientation=AngleAxis(Degrees(90), Axis{1.0,0.0,0.0}),
 	scale=.5,
 	Model([[component-models/HelpMenu/HelpMenu.ive]]),
 }
 
-ss = wiihelp:getOrCreateStateSet()
+local ss = wiihelp:getOrCreateStateSet()
 
 ss:setMode(gldef.GL_LIGHTING, osg.StateAttribute.Values.OFF)
 
