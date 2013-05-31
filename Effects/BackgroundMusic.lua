0689df9222bee7db9c308b17e25af2098bb00c58
Ryan Pavlik <rpavlik@iastate.edu>
2013-05-31 14:15:36 -0500

    Add the background music and attribution

--- Effects/BackgroundMusic.lua
+++ Effects/BackgroundMusic.lua
@@ -14,6 +14,9 @@ function startSound()
 
 	-- Loop
 	s:trigger(-1)
+
+	print("Music: Aquascape - Sunrise, on Tunguska Chillout Grooves Vol. 7 from the Tunguska Electronic Music Society")
+	print("Used under CC-BY-ND 3.0 - http://www.jamendo.com/en/track/761836/aquascape-sunrise")
 end
 
 print("Run 'startSound()' to begin the audio")
