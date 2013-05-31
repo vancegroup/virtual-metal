cd0ca8a4a289a87558a7049eea1236212453ac50
Ryan Pavlik <rpavlik@iastate.edu>
2013-05-31 13:52:49 -0500

    Add the "draw on top of everything" code.

--- Effects/HelpMenu.lua
+++ Effects/HelpMenu.lua
@@ -12,6 +12,17 @@ ss = wiihelp:getOrCreateStateSet()
 
 ss:setMode(gldef.GL_LIGHTING, osg.StateAttribute.Values.OFF)
 
+-- This line makes it so that it draws over everything (except apparently transparent stuff like the frusta)
+ss:setMode(gldef.GL_DEPTH_TEST, osg.StateAttribute.Values.OFF)
+
+-- This line makes it draw after the transparent things.
+ss:setRenderingHint(osg.StateSet.RenderingHint.TRANSPARENT_BIN)
+
+-- This changes the render order - see http://forum.openscenegraph.org/viewtopic.php?t=9884
+-- Not sure how this interacts with the above line.
+-- The number is just an arbitrarily large number, while RenderBin is the sorting method.
+ss:setRenderBinDetails(100, "RenderBin")
+
 Actions.addFrameAction(
 	function()
 		local wand = gadget.PositionInterface('VJWand')
